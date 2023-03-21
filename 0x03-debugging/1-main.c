392a312361d8828f09db46d003614c18a8ffc401
WolfShado <chamess.souib@gmail.com>
2023-03-21 15:45:30 -0700

    Editing the lines number

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,6 +3,12 @@
 * main - causes an infinite loop
 * Return: 0
 */
+
+
+
+
+
+
 int main(void)
 {
 int i;
@@ -13,5 +19,6 @@ i = 0;
 /*putchar(i);*/
 /*}*/
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
