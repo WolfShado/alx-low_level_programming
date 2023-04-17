7e5f54ac2238a8b611cae79471597e08c61b1b38
WolfShado <chamess.souib@gmail.com>
2023-04-17 06:56:57 -0700

    who lets the dog out

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog class
+ *
+ * @Name: Dog's Name 
+ *
+ * @Age: Dog's Age
+ *
+ * @Owner: Dog's Owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
