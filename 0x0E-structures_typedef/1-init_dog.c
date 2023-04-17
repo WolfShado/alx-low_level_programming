fdaffbbf66e021a98f606c51e4a733fa04a3fc17
WolfShado <chamess.souib@gmail.com>
2023-04-17 07:01:30 -0700

    betty check

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,12 +9,13 @@
  *
  * Return: void
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
 }
